23c46eddf074bcd88d169df268db4a764c159869
Russell Shurts <rshurts@users.noreply.github.com>
2017-05-17 18:31:47 -0500

    Proxy work

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ WORKDIR /app
 COPY app /app
 COPY cmd.sh /
 
-EXPOSE 9090 9091
+EXPOSE 9090 9191
 USER uwsgi
 
 CMD ["/cmd.sh"]
--- cmd.sh
+++ cmd.sh
@@ -9,5 +9,5 @@ elif [ "$ENV" = 'UNIT' ]; then
   exec python "tests.py"
 else
   echo "Running Production Server"
-  exec uwsgi --http 0.0.0.0:9000 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9091
+  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
